9af085db13ce8ff6ab2a0fa7d5468497dfd8c59a
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2024-09-27 08:37:07 -0300

    Correção de filtro dos estoques

--- Produto/models.py
+++ Produto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.forms import ModelForm
 from django import forms
 from Estoque.models import Estoque
+from datetime import date
 
 
 class Produto(models.Model):
@@ -12,20 +13,26 @@ class Produto(models.Model):
         ('CX', 'Caixa'),
         ('MT', 'Metro'),
     ]
-    
+
     descricao = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    unidade_medida = models.CharField(max_length=5, choices=UNIDADES_MEDIDA, blank=False, null=False)
+    unidade_medida = models.CharField(
+        max_length=5, choices=UNIDADES_MEDIDA, blank=False, null=False)
     quantidade = models.DecimalField(max_digits=10, decimal_places=1)
-    estoque_id = models.ForeignKey(Estoque, on_delete=models.CASCADE, related_name='produtos')  
+    estoque_id = models.ForeignKey(
+        Estoque, on_delete=models.CASCADE, related_name='produtos')
+    data_joined = models.DateField(
+        max_length=date.today(), default=date.today())
 
     def __str__(self):
         return self.descricao
 
+
 class CriaProduto(ModelForm):
     class Meta:
         model = Produto
-        fields = ['descricao', 'valor', 'unidade_medida', 'quantidade', 'estoque_id']
+        fields = ['descricao', 'valor',
+                  'unidade_medida', 'quantidade', 'estoque_id']
         widgets = {
             'descricao': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
             'valor': forms.NumberInput(attrs={'class': 'form-control'}),
@@ -35,5 +42,10 @@ class CriaProduto(ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
+        organizacao = kwargs.pop('organizacao', None)
         super().__init__(*args, **kwargs)
-        self.fields['estoque_id'].queryset = Estoque.objects.none()
+        if organizacao:
+            self.fields['estoque_id'].queryset = Estoque.objects.filter(
+                organizacao=organizacao)
+        else:
+            self.fields['estoque_id'].queryset = Estoque.objects.none()
--- Produto/views.py
+++ Produto/views.py
@@ -1,32 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from Produto.models import CriaProduto, Produto
 from Usuario.models import Colaborador
-from Produto.models import Estoque
-from django.contrib import messages
 
 
 def cria_produto_view(request):
     colaborador = Colaborador.objects.get(username=request.user)
-    estoque_organizacao = Estoque.objects.filter(organizacao=colaborador.organizacao)
     organizacao = colaborador.organizacao
     if request.method == 'POST':
-        form = CriaProduto(request.POST, request.POST, organizacao=organizacao)
+        form = CriaProduto(request.POST, organizacao=organizacao)
 
         if form.is_valid():
             form.save(commit=False)
 
-            form.estoque = form.cleaned_data['estoque']
-            form.organizacao = organizacao
             form.save()
             messages.success(request, 'Produto criado!')
             return redirect('estoque')
         else:
-            produto = form()
-            produto.fields['estoque'].queryset = estoque_organizacao
             messages.info(request, 'Erro ao criar o produto, tente novamente.')
 
     else:
-        form = CriaProduto()
+        form = CriaProduto(organizacao=organizacao)
 
     return render(request, 'produtos.html', {'form': form})
 

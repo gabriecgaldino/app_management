cfff9fb2649a12f8fe7ab1ebf409a3aa6a4355ee
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2024-11-26 22:35:09 -0300

    Correção dos imports em Usuario.models

--- Usuario/models.py
+++ Usuario/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth import get_user_model
 
 
 class Colaborador(AbstractUser):

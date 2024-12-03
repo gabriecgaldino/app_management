b04b3ba0162ee14ea8aa02fc1c182e156659ac85
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2024-12-03 15:06:00 -0300

    formuláeio de colaborador

--- Usuario/models.py
+++ Usuario/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from Organização.models import Empresa, Setor, Cargo
 
+
 class Endereco(models.Model):
     rua = models.CharField(max_length=100, blank=False)
     bairro = models.CharField(max_length=100, blank=False)
@@ -10,49 +11,51 @@ class Endereco(models.Model):
     pais = models.CharField(max_length=100, blank=False)
     cep = models.CharField(max_length=9, blank=False)
     numero = models.CharField(max_length=10)
-    
+
+
 class Colaborador(AbstractUser):
     # Dados pessoais
     cpf = models.CharField(max_length=14, unique=True)
     email = models.EmailField(max_length=50, unique=True)
     telefone = models.CharField(max_length=15, blank=True, null=True)
-    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT, blank=True, null=True)
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.PROTECT, blank=True, null=True)
     data_nascimento = models.DateField(blank=True, null=True)
 
     # Dados profissionais
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, blank=True, null=True, related_name='colaboradores')
-    setor = models.ForeignKey(Setor, on_delete=models.PROTECT, blank=True, null=True, related_name='colaboradores')
-    cargo = models.ForeignKey(Cargo, on_delete=models.PROTECT, blank=True, null=True, related_name='colaboradores')
+    empresa = models.ForeignKey(
+        Empresa, on_delete=models.PROTECT, blank=True, null=True, related_name='colaboradores')
+    setor = models.ForeignKey(Setor, on_delete=models.PROTECT,
+                              blank=True, null=True, related_name='colaboradores')
+    cargo = models.ForeignKey(Cargo, on_delete=models.PROTECT,
+                              blank=True, null=True, related_name='colaboradores')
     matricula = models.CharField(max_length=20, unique=True)
     data_demissao = models.DateField(blank=True, null=True)
-
 
     # Dados de acesso
     is_active = models.BooleanField(default=1)
 
     USERNAME_FIELD = 'username'
 
-    
-
-    def save(self, *args, **kwargs): 
+    def save(self, *args, **kwargs):
         self.is_active = 1
         if not self.username:
             self.username = self.cpf
+
         if not self.password:
             self.password1 = self.set_password(self.cpf[:8])
             self.password2 = self.set_password(self.cpf[:8])
 
+        if not self.matricula:
+            prefixo = self.empresa.nome_fantasia[:2].upper()
+            numero = str(Colaborador.objects.filter(
+                empresa=self.empresa).count() + 1).zfill(5)
+            self.matricula = f'{prefixo}{numero}'
+
         user = kwargs.pop('user', None)
 
         if user:
-            self.fields['empresa'].queryset = Empresa.objects.filter(gestor=user)
+            self.fields['empresa'].queryset = Empresa.objects.filter(
+                gestor=user)
+
         super().save(*args, **kwargs)
-
-
-        
-
-
-
-
-
-

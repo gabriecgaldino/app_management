b45f19a465844c26cb70bd0b25944b66ed6217d3
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2025-01-08 17:45:34 -0300

    Conclusão do modelo de formulário para atualização de dados

--- Configurações/static/js/edit_colaborador.js
+++ Configurações/static/js/edit_colaborador.js
@@ -6,8 +6,11 @@ document.querySelectorAll('.view-details').forEach(button=> {
             fetch(`/api/colaboradores/?colaboradorId=${colaboradorId}`)
             .then(res => res.json())
             .then(data => {
-                document.getElementById('nome').value = data.colaborador[0].first_name
-                document.getElementById('sobrenome').value = data.colaborador[0].last_name
+                document.getElementById('nome_edit').textContent = data.colaborador[0].first_name
+                document.getElementById('sobrenome_edit').value = data.colaborador[0].last_name
+                document.getElementById('cpf_edit').value = data.colaborador[0].cpf
+                document.getElementById('email_edit').value = data.colaborador[0].email
+                
 
 
                 

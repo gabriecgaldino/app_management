5bffe9b1d4889255056568d77ceb32ee5860ed42
Gabriel Galdino <gabrielcgaldiino@gmail.com>
2025-02-23 15:01:57 -0300

    Finalização dos principais recursos para implantação da loja no sistema.

--- Usuario/views.py
+++ Usuario/views.py
@@ -3,30 +3,37 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 import logging
 from .forms import LoginForm
+from .models import Colaborador
 
 logger = logging.getLogger(__name__)
 
 def login_view(request):
     form_login = LoginForm(request, data=request.POST or None)
+    
+    
 
     if request.method == 'POST':
         if form_login.is_valid():
             username = form_login.cleaned_data.get('username')
             password = form_login.cleaned_data.get('password')
+            
+            query_user = Colaborador.objects.get(username=username)
 
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                messages.success(request, f'Usuário autenticado com sucesso!')
-                return redirect('index')
-            else:
-                messages.error(request, "Usuário ou senha incorretos.")
+            if not query_user.is_developer:
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    login(request, user)
+                    messages.success(request, f'Usuário autenticado com sucesso!')
+                    return redirect('index')
+                else:
+                    messages.error(request, "Usuário ou senha incorretos.")
+            else: 
+                messages.warning(request, 'O acesso de desenvolvedor só está permitido na central do desenvolvedor.')
         else:
             messages.error(request, "Erro no preenchimento do formulário.")
             logger.error(f"Erro no formulário de login: {form_login.errors}")
 
     return render(request, 'login.html', {'form_login': form_login})
-        
         
 
 
